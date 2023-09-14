4160abfbffdf50ac6272554fc757daa63d194383
GGmaz <kajtes.nikola@gmail.com>
2023-09-14 13:43:00 +0200

    successfully created 2 teams and maybe lobby

--- cmd/main.go
+++ cmd/main.go
@@ -10,21 +10,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"github.com/paralin/go-dota2/protocol"
+	"google.golang.org/protobuf/proto"
 	"os"
+	"time"
 
 	"github.com/paralin/go-dota2"
-	"github.com/paralin/go-dota2/protocol"
 	"github.com/paralin/go-steam"
 	"github.com/paralin/go-steam/gsbot"
 	"github.com/paralin/go-steam/protocol/steamlang"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/protobuf/proto"
-	// "io/ioutil"
-	// "math/rand"
-	// "encoding/json"
-	// "github.com/paralin/go-steam/netutil"
 )
 
 func main() {
@@ -59,7 +55,7 @@ func main() {
 	}
 
 	details := &gsbot.LogOnDetails{
-		Username: os.Args[1], 
+		Username: os.Args[1],
 		Password: os.Args[2],
 		AuthCode: authcode,
 	}
@@ -75,23 +71,6 @@ func main() {
 	serverList := gsbot.NewServerList(bot, "serverlist.json")
 	serverList.Connect()
 
-	lobbyDetails := &protocol.CMsgPracticeLobbySetDetails{
-        GameName:           proto.String("Relative dota2 lobby"),
-        ServerRegion:       proto.Uint32(1), // Replace with the desired server region ID
-        GameMode:           proto.Uint32(2), // Replace with the desired game mode ID
-        AllowCheats:        proto.Bool(false),
-        FillWithBots:       proto.Bool(false),
-        AllowSpectating:    proto.Bool(true),
-        PassKey:            proto.String("mylobby123"), // Replace with your desired passkey
-        CustomGameMode:     proto.String("my_custom_game_mode"), // Replace with your custom game mode
-        CustomMapName:      proto.String("my_custom_map"),       // Replace with your custom map name
-        CustomMinPlayers:   proto.Uint32(2),                    // Replace with your desired minimum players
-        CustomMaxPlayers:   proto.Uint32(10),                   // Replace with your desired maximum players
-        CustomGameCrc:      proto.Uint64(123456789),            // Replace with your CRC value
-        CustomGameTimestamp: proto.Uint32(1234567890),          // Replace with your timestamp
-        LanHostPingLocation: proto.String("US West"),           // Replace with your desired LAN host ping location
-    }
-
 	for event := range client.Events() {
 		auth.HandleEvent(event)
 		debug.HandleEvent(event)
@@ -102,29 +81,67 @@ func main() {
 			fmt.Printf("Error: %v", e)
 		case *steam.LoggedOnEvent:
 			client.Social.SetPersonaState(steamlang.EPersonaState_Online)
-		
+
 		case *steam.PersonaStateEvent:
 			fmt.Printf("Successfully logged on as %s\n", e.Name) // Here it is connected to steam client
-
-			println("Connecting to dota2") 
+			println("Connecting to dota2")
 
 			dotaClient := dota2.New(client, logrus.New())
+			dotaClient.SetPlaying(true)
 
-			// log.Printf("Client: %v", dotaClient)
-			
-			// dotaClient.SendChannelMessage(123456780, "Relative bot is on Dota2")
-			// println("Sent message to channel")
-			
-			err := dotaClient.LeaveCreateLobby(context.Background(), lobbyDetails, true)
-			if err != nil {
-				println(err)
+			for i := 0; i < 10; i++ {
+				time.Sleep(1 * time.Second)
+				dotaClient.SayHello()
 			}
-			println("Lobby created")
+
+			//teamDetails := &protocol.CMsgDOTACreateTeam{
+			//	Name: proto.String("Example Team1"),
+			//	Tag:  proto.String("TAG1"),
+			//}
+			//
+			//team, err := dotaClient.CreateTeam(context.Background(), teamDetails)
+			//if err != nil {
+			//	log.Print(err)
+			//}
+			//log.Printf("Team created: %v", team)
+
+			cLobbyTeamDetails1 := protocol.CLobbyTeamDetails{
+				TeamId:       proto.Uint32(9187802),
+				TeamName:     proto.String("radiant"),
+				TeamTag:      proto.String("radiant"),
+				TeamComplete: proto.Bool(true),
+			}
+
+			cLobbyTeamDetails2 := protocol.CLobbyTeamDetails{
+				TeamId:       proto.Uint32(9187811),
+				TeamName:     proto.String("4glory"),
+				TeamTag:      proto.String("4glory"),
+				TeamComplete: proto.Bool(true),
+			}
+
+			lobbyDetails := &protocol.CMsgPracticeLobbySetDetails{
+				TeamDetails:         []*protocol.CLobbyTeamDetails{&cLobbyTeamDetails1, &cLobbyTeamDetails2},
+				GameName:            proto.String("Relative dota2 lobby"),
+				ServerRegion:        proto.Uint32(1), // Replace with the desired server region ID
+				GameMode:            proto.Uint32(2), // Replace with the desired game mode ID
+				AllowCheats:         proto.Bool(false),
+				FillWithBots:        proto.Bool(false),
+				AllowSpectating:     proto.Bool(true),
+				PassKey:             proto.String("mylobby123"),          // Replace with your desired passkey
+				CustomGameMode:      proto.String("my_custom_game_mode"), // Replace with your custom game mode
+				CustomMapName:       proto.String("my_custom_map"),       // Replace with your custom map name
+				CustomMinPlayers:    proto.Uint32(2),                     // Replace with your desired minimum players
+				CustomMaxPlayers:    proto.Uint32(10),                    // Replace with your desired maximum players
+				CustomGameCrc:       proto.Uint64(123456789),             // Replace with your CRC value
+				CustomGameTimestamp: proto.Uint32(1234567890),            // Replace with your timestamp
+				LanHostPingLocation: proto.String("US West"),             // Replace with your desired LAN host ping location
+			}
+
+			dotaClient.CreateLobby(lobbyDetails)
 
 			dotaClient.Close()
-
 			return // exit
 		}
 	}
-	
+
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/paralin/go-dota2 v0.0.0-20230812030130-d63690b89ff0
 	github.com/paralin/go-steam v0.0.0-20230323124938-41e2a40a56ff
 	github.com/sirupsen/logrus v1.9.3
+	google.golang.org/protobuf v1.31.0
 )
 
 require (
@@ -14,5 +15,4 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.8.3 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
 )
