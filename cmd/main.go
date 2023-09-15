404d20b2f86ded42ad62c4e83e1170115ecb81f2
Filip Pinjuh <filip.pinjuh@gmail.com>
2023-09-15 15:48:45 +0200

    successfully inviting and launching lobby

--- cmd/main.go
+++ cmd/main.go
@@ -11,12 +11,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/paralin/go-dota2/protocol"
-	"google.golang.org/protobuf/proto"
+	"log"
 	"os"
 	"time"
 
 	"github.com/paralin/go-dota2"
+	"github.com/paralin/go-dota2/cso"
 	"github.com/paralin/go-steam"
 	"github.com/paralin/go-steam/gsbot"
 	"github.com/paralin/go-steam/protocol/steamlang"
@@ -24,19 +24,6 @@ import (
 )
 
 func main() {
-	// d, err := ioutil.ReadFile("serverlist.json")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// println(string(d))
-
-	// var addrs []*netutil.PortAddr
-	// err = json.Unmarshal(d, &addrs)
-	// if err != nil {
-	// 	println(err.Error())
-	// }
-	// raddr := addrs[rand.Intn(len(addrs))]
-	// println(raddr.String())
 
 	if len(os.Args) < 3 {
 		fmt.Println("gsbot example\nusage: \n\tgsbot [username] [password] [authcode]")
@@ -84,64 +71,40 @@ func main() {
 
 		case *steam.PersonaStateEvent:
 			fmt.Printf("Successfully logged on as %s\n", e.Name) // Here it is connected to steam client
-			println("Connecting to dota2")
-
-			dotaClient := dota2.New(client, logrus.New())
-			dotaClient.SetPlaying(true)
-
-			for i := 0; i < 10; i++ {
-				time.Sleep(1 * time.Second)
-				dotaClient.SayHello()
-			}
-
-			//teamDetails := &protocol.CMsgDOTACreateTeam{
-			//	Name: proto.String("Example Team1"),
-			//	Tag:  proto.String("TAG1"),
-			//}
-			//
-			//team, err := dotaClient.CreateTeam(context.Background(), teamDetails)
-			//if err != nil {
-			//	log.Print(err)
-			//}
-			//log.Printf("Team created: %v", team)
-
-			cLobbyTeamDetails1 := protocol.CLobbyTeamDetails{
-				TeamId:       proto.Uint32(9187802),
-				TeamName:     proto.String("radiant"),
-				TeamTag:      proto.String("radiant"),
-				TeamComplete: proto.Bool(true),
-			}
-
-			cLobbyTeamDetails2 := protocol.CLobbyTeamDetails{
-				TeamId:       proto.Uint32(9187811),
-				TeamName:     proto.String("4glory"),
-				TeamTag:      proto.String("4glory"),
-				TeamComplete: proto.Bool(true),
-			}
-
-			lobbyDetails := &protocol.CMsgPracticeLobbySetDetails{
-				TeamDetails:         []*protocol.CLobbyTeamDetails{&cLobbyTeamDetails1, &cLobbyTeamDetails2},
-				GameName:            proto.String("Relative dota2 lobby"),
-				ServerRegion:        proto.Uint32(1), // Replace with the desired server region ID
-				GameMode:            proto.Uint32(2), // Replace with the desired game mode ID
-				AllowCheats:         proto.Bool(false),
-				FillWithBots:        proto.Bool(false),
-				AllowSpectating:     proto.Bool(true),
-				PassKey:             proto.String("mylobby123"),          // Replace with your desired passkey
-				CustomGameMode:      proto.String("my_custom_game_mode"), // Replace with your custom game mode
-				CustomMapName:       proto.String("my_custom_map"),       // Replace with your custom map name
-				CustomMinPlayers:    proto.Uint32(2),                     // Replace with your desired minimum players
-				CustomMaxPlayers:    proto.Uint32(10),                    // Replace with your desired maximum players
-				CustomGameCrc:       proto.Uint64(123456789),             // Replace with your CRC value
-				CustomGameTimestamp: proto.Uint32(1234567890),            // Replace with your timestamp
-				LanHostPingLocation: proto.String("US West"),             // Replace with your desired LAN host ping location
-			}
-
-			dotaClient.CreateLobby(lobbyDetails)
-
-			dotaClient.Close()
-			return // exit
+			Connect2Dota(client)
 		}
 	}
+}
 
+func Connect2Dota(client *steam.Client) {
+
+	println("Connecting to dota2")
+
+	dotaClient := dota2.New(client, logrus.New())
+	dotaClient.SetPlaying(true)
+
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+		dotaClient.SayHello()
+	}
+
+	// SOCACHE MECHANISM
+	eventCh, eventCancel, err:= dotaClient.GetCache().SubscribeType(cso.Lobby) // Listen to lobby cache
+	if err != nil {
+		log.Fatalf("Failed to subscribe to lobby cache: %v", err)
+	}
+
+	dotaClient.InviteLobbyMember(76561198153440660) 
+	defer eventCancel()
+
+	lobbyEvent := <-eventCh
+	lobby := lobbyEvent.Object.String()
+	log.Printf("Lobby: %v", lobby)
+
+	time.Sleep(30 * time.Second)
+
+	dotaClient.LaunchLobby()
+	println("Launched lobby")
+
+	time.Sleep(30 * time.Second)
 }
